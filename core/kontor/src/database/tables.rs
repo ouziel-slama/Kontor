cbe8774f2a8a69026ee0bf1b4e53bb0f8490f123
wilfreddenton <wilfred@hey.com>
2025-05-30 11:00:43 -0400

    sqlean ext multi platform support

--- core/kontor/src/database/tables.rs
+++ core/kontor/src/database/tables.rs
@@ -1,14 +1,33 @@
+use std::env;
+
 pub const CREATE_BLOCKS_TABLE: &str = "
     CREATE TABLE IF NOT EXISTS blocks (
         height INTEGER PRIMARY KEY,
         hash TEXT NOT NULL
     )";
 
+fn get_sqlean_ext_path(module: &str) -> String {
+    let os = env::consts::OS;
+    let arch = env::consts::ARCH;
+
+    let platform = match (os, arch) {
+        ("macos", "aarch64") => "macos-arm64",
+        ("macos", "x86_64") => "macos-x86",
+        ("linux", "x86_64") => "linux-x86",
+        ("linux", "aarch64") => "linux-arm64",
+        _ => panic!("Unsupported platform or architecture"),
+    };
+
+    let extension = if os == "macos" { "dylib" } else { "so" };
+
+    format!("sqlean-0.27.2/{}/{}.{}", platform, module, extension)
+}
+
 pub async fn initialize_database(conn: &libsql::Connection) -> Result<(), libsql::Error> {
     conn.execute(CREATE_BLOCKS_TABLE, ()).await?;
     conn.query("PRAGMA journal_mode = WAL;", ()).await?;
     conn.query("PRAGMA synchronous = NORMAL;", ()).await?;
     conn.load_extension_enable()?;
-    conn.load_extension("sqlean/crypto.dylib", None)?;
+    conn.load_extension(get_sqlean_ext_path("crypto"), None)?;
     Ok(())
 }
