66cb677f0d46b550af8a63a6fe933aa17c96961e
wilfreddenton <wilfred@hey.com>
2025-06-27 09:45:00 -0400

    rename packages

---
+++ core/indexer/tests/reactor.rs
@@ -0,0 +1,508 @@
+use anyhow::{Result, anyhow};
+use clap::Parser;
+use libsql::Connection;
+use tokio::time::{Duration, sleep};
+use tokio_util::sync::CancellationToken;
+
+use bitcoin::{BlockHash, hashes::Hash};
+
+use indexer::{
+    bitcoin_follower::{
+        ctrl::CtrlChannel,
+        events::{BlockId, Event},
+    },
+    block::{Block, Tx},
+    config::Config,
+    database::{queries, types::BlockRow},
+    reactor,
+    retry::{new_backoff_unlimited, retry},
+    utils::{MockTransaction, new_test_db},
+};
+
+fn gen_block<T: Tx>(height: u64, hash: &BlockHash, prev_hash: &BlockHash) -> Block<T> {
+    Block {
+        height,
+        hash: *hash,
+        prev_hash: *prev_hash,
+        transactions: vec![],
+    }
+}
+
+fn gen_blocks<T: Tx>(start: u64, end: u64, prev_hash: BlockHash) -> Vec<Block<T>> {
+    let mut blocks = vec![];
+    let mut prev = prev_hash;
+
+    for _i in start..end {
+        let hash = BlockHash::from_byte_array([(_i + 1) as u8; 32]);
+        let block = gen_block(_i + 1, &hash, &prev);
+        blocks.push(block.clone());
+
+        prev = hash;
+    }
+
+    blocks
+}
+
+fn new_block_chain<T: Tx>(n: u64) -> Vec<Block<T>> {
+    gen_blocks(0, n, BlockHash::from_byte_array([0x00; 32]))
+}
+
+fn block_row<T: Tx>(height: u64, b: &Block<T>) -> BlockRow {
+    BlockRow {
+        height,
+        hash: b.hash,
+    }
+}
+
+async fn select_block_at_height(
+    conn: &Connection,
+    height: u64,
+    cancel_token: CancellationToken,
+) -> Result<BlockRow> {
+    retry(
+        async || match queries::select_block_at_height(conn, height).await {
+            Ok(Some(row)) => Ok(row),
+            Ok(None) => Err(anyhow!("Block at height not found: {}", height)),
+            Err(e) => Err(e.into()),
+        },
+        "read block at height",
+        new_backoff_unlimited(),
+        cancel_token.clone(),
+    )
+    .await
+}
+
+#[tokio::test]
+async fn test_reactor_rollback_event() -> Result<()> {
+    let cancel_token = CancellationToken::new();
+    let (ctrl, mut ctrl_rx) = CtrlChannel::create();
+    let (reader, writer, _temp_dir) = new_test_db(&Config::try_parse()?).await?;
+
+    let handle = reactor::run::<MockTransaction>(
+        91,
+        cancel_token.clone(),
+        reader.clone(),
+        writer.clone(),
+        ctrl,
+    );
+
+    let start = ctrl_rx.recv().await.unwrap();
+    assert_eq!(start.start_height, 91);
+    let tx = start.event_tx;
+
+    assert!(
+        tx.send(Event::BlockInsert((
+            100,
+            Block {
+                height: 91,
+                hash: BlockHash::from_byte_array([0x10; 32]),
+                prev_hash: BlockHash::from_byte_array([0x00; 32]),
+                transactions: vec![],
+            },
+        )))
+        .await
+        .is_ok()
+    );
+
+    assert!(
+        tx.send(Event::BlockInsert((
+            100,
+            Block {
+                height: 92,
+                hash: BlockHash::from_byte_array([0x20; 32]),
+                prev_hash: BlockHash::from_byte_array([0x10; 32]),
+                transactions: vec![],
+            },
+        )))
+        .await
+        .is_ok()
+    );
+
+    assert!(
+        tx.send(Event::BlockInsert((
+            100,
+            Block {
+                height: 93,
+                hash: BlockHash::from_byte_array([0x30; 32]),
+                prev_hash: BlockHash::from_byte_array([0x20; 32]),
+                transactions: vec![],
+            },
+        )))
+        .await
+        .is_ok()
+    );
+
+    sleep(Duration::from_millis(10)).await; // short delay to hopefully avoid a read retry
+    let conn = &*reader.connection().await?;
+    let block = select_block_at_height(conn, 92, cancel_token.clone()).await?;
+    assert_eq!(block.height, 92);
+    assert_eq!(block.hash, BlockHash::from_byte_array([0x20; 32]));
+
+    assert!(
+        tx.send(Event::BlockRemove(BlockId::Height(91)))
+            .await
+            .is_ok()
+    );
+
+    let start = ctrl_rx.recv().await.unwrap();
+    assert_eq!(start.start_height, 92);
+    assert_eq!(
+        start.last_hash,
+        Some(BlockHash::from_byte_array([0x10; 32]))
+    );
+    let tx = start.event_tx;
+
+    assert!(
+        tx.send(Event::BlockInsert((
+            100,
+            Block {
+                height: 92,
+                hash: BlockHash::from_byte_array([0x21; 32]),
+                prev_hash: BlockHash::from_byte_array([0x10; 32]),
+                transactions: vec![],
+            },
+        )))
+        .await
+        .is_ok()
+    );
+
+    assert!(
+        tx.send(Event::BlockInsert((
+            100,
+            Block {
+                height: 93,
+                hash: BlockHash::from_byte_array([0x31; 32]),
+                prev_hash: BlockHash::from_byte_array([0x21; 32]),
+                transactions: vec![],
+            },
+        )))
+        .await
+        .is_ok()
+    );
+
+    sleep(Duration::from_millis(10)).await; // short delay to hopefully avoid a read retry
+
+    let block = select_block_at_height(conn, 92, cancel_token.clone()).await?;
+    assert_eq!(block.height, 92);
+    assert_eq!(block.hash, BlockHash::from_byte_array([0x21; 32]));
+
+    let block = select_block_at_height(conn, 93, cancel_token.clone()).await?;
+    assert_eq!(block.height, 93);
+    assert_eq!(block.hash, BlockHash::from_byte_array([0x31; 32]));
+
+    assert!(!handle.is_finished());
+
+    cancel_token.cancel();
+    let _ = handle.await;
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_reactor_unexpected_block() -> Result<()> {
+    let cancel_token = CancellationToken::new();
+    let (ctrl, mut ctrl_rx) = CtrlChannel::create();
+    let (reader, writer, _temp_dir) = new_test_db(&Config::try_parse()?).await?;
+
+    let handle = reactor::run::<MockTransaction>(
+        81,
+        cancel_token.clone(),
+        reader.clone(),
+        writer.clone(),
+        ctrl,
+    );
+
+    let start = ctrl_rx.recv().await.unwrap();
+    assert_eq!(start.start_height, 81);
+    let tx = start.event_tx;
+
+    assert!(
+        tx.send(Event::BlockInsert((
+            100,
+            Block {
+                height: 82, // skipping 81
+                hash: BlockHash::from_byte_array([0x01; 32]),
+                prev_hash: BlockHash::from_byte_array([0x00; 32]),
+                transactions: vec![],
+            },
+        )))
+        .await
+        .is_ok()
+    );
+
+    cancel_token.cancelled().await;
+    assert!(cancel_token.is_cancelled());
+
+    let _ = handle.await;
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_reactor_rollback_due_to_hash_mismatch() -> Result<()> {
+    let cancel_token = CancellationToken::new();
+    let (ctrl, mut ctrl_rx) = CtrlChannel::create();
+    let (reader, writer, _temp_dir) = new_test_db(&Config::try_parse()?).await?;
+
+    let handle = reactor::run::<MockTransaction>(
+        91,
+        cancel_token.clone(),
+        reader.clone(),
+        writer.clone(),
+        ctrl,
+    );
+
+    let start = ctrl_rx.recv().await.unwrap();
+    assert_eq!(start.start_height, 91);
+    let tx = start.event_tx;
+
+    assert!(
+        tx.send(Event::BlockInsert((
+            100,
+            Block {
+                height: 91,
+                hash: BlockHash::from_byte_array([0x01; 32]),
+                prev_hash: BlockHash::from_byte_array([0x00; 32]),
+                transactions: vec![],
+            },
+        )))
+        .await
+        .is_ok()
+    );
+
+    assert!(
+        tx.send(Event::BlockInsert((
+            100,
+            Block {
+                height: 92,
+                hash: BlockHash::from_byte_array([0x02; 32]),
+                prev_hash: BlockHash::from_byte_array([0x01; 32]),
+                transactions: vec![],
+            },
+        )))
+        .await
+        .is_ok()
+    );
+
+    sleep(Duration::from_millis(10)).await; // short delay to hopefully avoid a read retry
+
+    let conn = &*reader.connection().await?;
+    let block = select_block_at_height(conn, 92, cancel_token.clone()).await?;
+    assert_eq!(block.height, 92);
+    assert_eq!(block.hash, BlockHash::from_byte_array([0x02; 32]));
+
+    assert!(
+        tx.send(Event::BlockInsert((
+            100,
+            Block {
+                height: 93,
+                hash: BlockHash::from_byte_array([0x03; 32]),
+                prev_hash: BlockHash::from_byte_array([0x12; 32]), // not matching
+                transactions: vec![],
+            },
+        )))
+        .await
+        .is_ok()
+    );
+
+    let start = ctrl_rx.recv().await.unwrap();
+    assert_eq!(start.start_height, 92);
+    assert_eq!(
+        start.last_hash,
+        Some(BlockHash::from_byte_array([0x01; 32]))
+    );
+
+    let tx = start.event_tx;
+
+    assert!(
+        tx.send(Event::BlockInsert((
+            100,
+            Block {
+                height: 92,
+                hash: BlockHash::from_byte_array([0x12; 32]),
+                prev_hash: BlockHash::from_byte_array([0x01; 32]),
+                transactions: vec![],
+            },
+        )))
+        .await
+        .is_ok()
+    );
+
+    sleep(Duration::from_millis(10)).await; // short delay to hopefully avoid a read retry
+    let block = select_block_at_height(conn, 92, cancel_token.clone()).await?;
+    assert_eq!(block.height, 92);
+    assert_eq!(block.hash, BlockHash::from_byte_array([0x12; 32]));
+
+    assert!(!handle.is_finished());
+
+    cancel_token.cancel();
+    let _ = handle.await;
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_reactor_rollback_due_to_reverting_height() -> Result<()> {
+    let cancel_token = CancellationToken::new();
+    let (ctrl, mut ctrl_rx) = CtrlChannel::create();
+    let (reader, writer, _temp_dir) = new_test_db(&Config::try_parse()?).await?;
+
+    let handle = reactor::run::<MockTransaction>(
+        91,
+        cancel_token.clone(),
+        reader.clone(),
+        writer.clone(),
+        ctrl,
+    );
+
+    let start = ctrl_rx.recv().await.unwrap();
+    assert_eq!(start.start_height, 91);
+    let tx = start.event_tx;
+
+    assert!(
+        tx.send(Event::BlockInsert((
+            100,
+            Block {
+                height: 91,
+                hash: BlockHash::from_byte_array([0x01; 32]),
+                prev_hash: BlockHash::from_byte_array([0x00; 32]),
+                transactions: vec![],
+            },
+        )))
+        .await
+        .is_ok()
+    );
+
+    assert!(
+        tx.send(Event::BlockInsert((
+            100,
+            Block {
+                height: 92,
+                hash: BlockHash::from_byte_array([0x02; 32]),
+                prev_hash: BlockHash::from_byte_array([0x01; 32]),
+                transactions: vec![],
+            },
+        )))
+        .await
+        .is_ok()
+    );
+
+    assert!(
+        tx.send(Event::BlockInsert((
+            100,
+            Block {
+                height: 93,
+                hash: BlockHash::from_byte_array([0x03; 32]),
+                prev_hash: BlockHash::from_byte_array([0x02; 32]),
+                transactions: vec![],
+            },
+        )))
+        .await
+        .is_ok()
+    );
+
+    assert!(
+        tx.send(Event::BlockInsert((
+            100,
+            Block {
+                height: 92,                                   // lower height
+                hash: BlockHash::from_byte_array([0x12; 32]), // new hash
+                prev_hash: BlockHash::from_byte_array([0x01; 32]),
+                transactions: vec![],
+            },
+        )))
+        .await
+        .is_ok()
+    );
+
+    // we're re-requesting the block we just received, which is wasteful but
+    // it doesn't seem worth having a special code-path for what should be
+    // an exceptional case.
+
+    let start = ctrl_rx.recv().await.unwrap();
+    assert_eq!(start.start_height, 92);
+    assert_eq!(
+        start.last_hash,
+        Some(BlockHash::from_byte_array([0x01; 32]))
+    );
+    let tx = start.event_tx;
+
+    assert!(
+        tx.send(Event::BlockInsert((
+            100,
+            Block {
+                height: 92,
+                hash: BlockHash::from_byte_array([0x12; 32]),
+                prev_hash: BlockHash::from_byte_array([0x01; 32]),
+                transactions: vec![],
+            },
+        )))
+        .await
+        .is_ok()
+    );
+
+    let conn = &*reader.connection().await?;
+    sleep(Duration::from_millis(10)).await; // short delay to hopefully avoid a read retry
+    let block = select_block_at_height(conn, 92, cancel_token.clone()).await?;
+    assert_eq!(block.height, 92);
+    assert_eq!(block.hash, BlockHash::from_byte_array([0x12; 32]));
+
+    assert!(!handle.is_finished());
+
+    cancel_token.cancel();
+    let _ = handle.await;
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_reactor_rollback_hash_event() -> Result<()> {
+    let cancel_token = CancellationToken::new();
+    let (ctrl, mut ctrl_rx) = CtrlChannel::create();
+    let (reader, writer, _temp_dir) = new_test_db(&Config::try_parse()?).await?;
+
+    let blocks = new_block_chain::<MockTransaction>(5);
+    let conn = &writer.connection();
+    assert!(
+        queries::insert_block(conn, block_row(1, &blocks[1 - 1]))
+            .await
+            .is_ok()
+    );
+    assert!(
+        queries::insert_block(conn, block_row(2, &blocks[2 - 1]))
+            .await
+            .is_ok()
+    );
+    assert!(
+        queries::insert_block(conn, block_row(3, &blocks[3 - 1]))
+            .await
+            .is_ok()
+    );
+
+    let handle = reactor::run::<MockTransaction>(
+        4,
+        cancel_token.clone(),
+        reader.clone(),
+        writer.clone(),
+        ctrl,
+    );
+
+    let start = ctrl_rx.recv().await.unwrap();
+    assert_eq!(start.start_height, 4);
+    assert_eq!(start.last_hash, Some(blocks[3 - 1].hash));
+    let tx = start.event_tx;
+
+    assert!(
+        tx.send(Event::BlockRemove(BlockId::Hash(blocks[2 - 1].hash)))
+            .await
+            .is_ok()
+    );
+
+    let start = ctrl_rx.recv().await.unwrap();
+    assert_eq!(start.start_height, 2);
+    assert_eq!(start.last_hash, Some(blocks[1 - 1].hash));
+    assert!(!handle.is_finished());
+    cancel_token.cancel();
+    let _ = handle.await;
+    Ok(())
+}
---
+++ core/indexer/tests/swap.rs
@@ -0,0 +1,331 @@
+use std::str::FromStr;
+
+use anyhow::Result;
+use bitcoin::Amount;
+use bitcoin::FeeRate;
+use bitcoin::OutPoint;
+use bitcoin::Psbt;
+use bitcoin::Transaction;
+use bitcoin::TxIn;
+use bitcoin::TxOut;
+use bitcoin::Txid;
+use bitcoin::XOnlyPublicKey;
+use bitcoin::absolute::LockTime;
+use bitcoin::psbt::Input;
+use bitcoin::psbt::Output;
+use bitcoin::script::Instruction;
+use bitcoin::secp256k1::Keypair;
+use bitcoin::taproot::LeafVersion;
+use bitcoin::taproot::TaprootBuilder;
+use bitcoin::transaction::Version;
+use bitcoin::{
+    ScriptBuf,
+    address::{Address, KnownHrp},
+    consensus::encode::serialize as serialize_tx,
+    key::Secp256k1,
+};
+use clap::Parser;
+use indexer::api::compose::ComposeInputs;
+use indexer::api::compose::RevealInputs;
+use indexer::api::compose::compose;
+use indexer::api::compose::compose_reveal;
+use indexer::config::TestConfig;
+use indexer::op_return::OpReturnData;
+use indexer::test_utils;
+use indexer::witness_data::TokenBalance;
+use indexer::witness_data::WitnessData;
+use indexer::{bitcoin_client::Client, config::Config};
+
+#[tokio::test]
+async fn test_psbt_inscription() -> Result<()> {
+    let client = Client::new_from_config(&Config::try_parse()?)?;
+    let config = TestConfig::try_parse()?;
+
+    let secp = Secp256k1::new();
+
+    let (seller_address, seller_child_key, _) =
+        test_utils::generate_taproot_address_from_mnemonic(&secp, &config, 0)?;
+
+    let (buyer_address, buyer_child_key, _) =
+        test_utils::generate_taproot_address_from_mnemonic(&secp, &config, 1)?;
+
+    let keypair = Keypair::from_secret_key(&secp, &seller_child_key.private_key);
+    let (internal_key, _parity) = keypair.x_only_public_key();
+
+    let token_value = 1000;
+    let attach_witness_data = WitnessData::Attach {
+        output_index: 0,
+        token_balance: TokenBalance {
+            value: token_value,
+            name: "token_name".to_string(),
+        },
+    };
+
+    let mut serialized_token_balance = Vec::new();
+    ciborium::into_writer(&attach_witness_data, &mut serialized_token_balance).unwrap();
+
+    let detach_data = WitnessData::Detach {
+        output_index: 0,
+        token_balance: TokenBalance {
+            value: token_value,
+            name: "token_name".to_string(),
+        },
+    };
+    let mut serialized_detach_data = Vec::new();
+    ciborium::into_writer(&detach_data, &mut serialized_detach_data).unwrap();
+
+    let outpoint = OutPoint {
+        txid: Txid::from_str("dd3d962f95741f2f5c3b87d6395c325baa75c4f3f04c7652e258f6005d70f3e8")?,
+        vout: 0,
+    };
+
+    let txout = TxOut {
+        value: Amount::from_sat(9000),
+        script_pubkey: seller_address.script_pubkey(),
+    };
+
+    let compose_params = ComposeInputs::builder()
+        .address(seller_address.clone())
+        .x_only_public_key(internal_key)
+        .funding_utxos(vec![(outpoint, txout)])
+        .script_data(serialized_token_balance)
+        .fee_rate(FeeRate::from_sat_per_vb(2).unwrap())
+        .chained_script_data(serialized_detach_data.clone())
+        .envelope(546)
+        .build();
+
+    let compose_outputs = compose(compose_params)?;
+    let mut attach_commit_tx = compose_outputs.commit_transaction;
+    let mut attach_reveal_tx = compose_outputs.reveal_transaction;
+    let attach_tap_script = compose_outputs.tap_script;
+    let detach_tap_script = compose_outputs.chained_tap_script.unwrap();
+
+    let prevouts = vec![TxOut {
+        value: Amount::from_sat(9000), // existing utxo with 9000 sats
+        script_pubkey: seller_address.script_pubkey(),
+    }];
+
+    test_utils::sign_key_spend(&secp, &mut attach_commit_tx, &prevouts, &keypair, 0)?;
+
+    let attach_taproot_spend_info = TaprootBuilder::new()
+        .add_leaf(0, attach_tap_script.clone())
+        .expect("Failed to add leaf")
+        .finalize(&secp, internal_key)
+        .expect("Failed to finalize Taproot tree");
+
+    let prevouts = vec![attach_commit_tx.output[0].clone()];
+
+    test_utils::sign_script_spend(
+        &secp,
+        &attach_taproot_spend_info,
+        &attach_tap_script,
+        &mut attach_reveal_tx,
+        &prevouts,
+        &keypair,
+        0,
+    )?;
+
+    let attach_reveal_witness = attach_reveal_tx.input[0].witness.clone();
+    // Get the script from the witness
+    let script_bytes = attach_reveal_witness.to_vec()[1].clone();
+    let script = ScriptBuf::from_bytes(script_bytes);
+
+    // Parse the script instructions
+    let instructions = script.instructions().collect::<Result<Vec<_>, _>>()?;
+
+    if let [
+        Instruction::PushBytes(key),
+        _,
+        _,
+        _,
+        _,
+        _,
+        Instruction::PushBytes(serialized_data),
+        _,
+    ] = instructions.as_slice()
+    {
+        let witness_data: WitnessData = ciborium::from_reader(serialized_data.as_bytes())?;
+        assert_eq!(witness_data, attach_witness_data);
+
+        if let WitnessData::Attach {
+            token_balance,
+            output_index,
+        } = witness_data
+        {
+            let detach_data = WitnessData::Detach {
+                output_index,
+                token_balance,
+            };
+            let secp = Secp256k1::new();
+            let mut serialized_detach_data = Vec::new();
+            ciborium::into_writer(&detach_data, &mut serialized_detach_data).unwrap();
+
+            let x_only_public_key = XOnlyPublicKey::from_slice(key.as_bytes())?;
+            let detach_tap_script = test_utils::build_inscription(
+                serialized_detach_data,
+                test_utils::PublicKey::Taproot(&x_only_public_key),
+            )?;
+
+            let detach_spend_info = TaprootBuilder::new()
+                .add_leaf(0, detach_tap_script)
+                .expect("Failed to add leaf")
+                .finalize(&secp, x_only_public_key)
+                .expect("Failed to finalize Taproot tree");
+
+            let detach_script_address_2 =
+                Address::p2tr_tweaked(detach_spend_info.output_key(), KnownHrp::Mainnet);
+
+            assert_eq!(
+                detach_script_address_2.script_pubkey(),
+                attach_reveal_tx.output[0].script_pubkey
+            );
+        } else {
+            panic!("Invalid witness data");
+        }
+    } else {
+        panic!("Invalid script instructions");
+    }
+    let detach_tapscript_spend_info = TaprootBuilder::new()
+        .add_leaf(0, detach_tap_script.clone())
+        .expect("Failed to add leaf")
+        .finalize(&secp, internal_key)
+        .expect("Failed to finalize Taproot tree");
+
+    let detach_control_block = detach_tapscript_spend_info
+        .control_block(&(detach_tap_script.clone(), LeafVersion::TapScript))
+        .expect("Failed to create control block");
+
+    let mut seller_detach_psbt = Psbt {
+        unsigned_tx: Transaction {
+            version: Version(2),
+            lock_time: LockTime::ZERO,
+            input: vec![TxIn {
+                previous_output: OutPoint {
+                    txid: attach_reveal_tx.compute_txid(),
+                    vout: 0,
+                },
+                ..Default::default()
+            }],
+            output: vec![TxOut {
+                value: Amount::from_sat(600), // price
+                script_pubkey: seller_address.script_pubkey(),
+            }],
+        },
+        inputs: vec![Input {
+            witness_utxo: Some(attach_reveal_tx.output[0].clone()),
+            tap_internal_key: Some(internal_key),
+            tap_merkle_root: Some(detach_tapscript_spend_info.merkle_root().unwrap()),
+            tap_scripts: {
+                let mut scripts = std::collections::BTreeMap::new();
+                scripts.insert(
+                    detach_control_block.clone(),
+                    (detach_tap_script.clone(), LeafVersion::TapScript),
+                );
+                scripts
+            },
+            ..Default::default()
+        }],
+        outputs: vec![Output::default()],
+        version: 0,
+        xpub: Default::default(),
+        proprietary: Default::default(),
+        unknown: Default::default(),
+    };
+    let prevouts = vec![attach_reveal_tx.output[0].clone()];
+    test_utils::sign_seller_side_psbt(
+        &secp,
+        &mut seller_detach_psbt,
+        &detach_tap_script,
+        internal_key,
+        detach_control_block,
+        &keypair,
+        &prevouts,
+    );
+
+    let buyer_keypair = Keypair::from_secret_key(&secp, &buyer_child_key.private_key);
+    let (buyer_internal_key, _) = buyer_keypair.x_only_public_key();
+
+    // Create transfer data pointing to output 2 (buyer's address)
+    let transfer_data = OpReturnData::D {
+        destination: buyer_internal_key,
+    };
+    let mut transfer_bytes = Vec::new();
+    ciborium::into_writer(&transfer_data, &mut transfer_bytes).unwrap();
+
+    let reveal_inputs = RevealInputs::builder()
+        .x_only_public_key(buyer_internal_key)
+        .address(buyer_address.clone())
+        .commit_output((
+            OutPoint {
+                txid: attach_reveal_tx.compute_txid(),
+                vout: 0,
+            },
+            attach_reveal_tx.output[0].clone(),
+        ))
+        .funding_utxos(vec![(
+            OutPoint {
+                txid: Txid::from_str(
+                    "ffb32fce7a4ce109ed2b4b02de910ea1a08b9017d88f1da7f49b3d2f79638cc3",
+                )?,
+                vout: 0,
+            },
+            TxOut {
+                value: Amount::from_sat(10000),
+                script_pubkey: buyer_address.script_pubkey(),
+            },
+        )])
+        .commit_script_data(serialized_detach_data)
+        .reveal_output(TxOut {
+            value: Amount::from_sat(600),
+            script_pubkey: seller_address.script_pubkey(),
+        })
+        .op_return_data(transfer_bytes)
+        .fee_rate(FeeRate::from_sat_per_vb(2).unwrap())
+        .envelope(546)
+        .build();
+    let buyer_reveal_outputs = compose_reveal(reveal_inputs)?;
+
+    // Create buyer's PSBT that combines with seller's PSBT
+    let mut buyer_psbt = buyer_reveal_outputs.psbt;
+
+    buyer_psbt.inputs[0] = seller_detach_psbt.inputs[0].clone();
+    buyer_psbt.inputs[1].witness_utxo = Some(TxOut {
+        script_pubkey: buyer_address.script_pubkey(),
+        value: Amount::from_sat(10000),
+    });
+    buyer_psbt.inputs[1].tap_internal_key = Some(buyer_internal_key);
+
+    // Define the prevouts explicitly in the same order as inputs
+    let prevouts = [
+        attach_reveal_tx.output[0].clone(),
+        TxOut {
+            value: Amount::from_sat(10000), // The value of the second input (buyer's UTXO)
+            script_pubkey: buyer_address.script_pubkey(),
+        },
+    ];
+
+    test_utils::sign_buyer_side_psbt(&secp, &mut buyer_psbt, &buyer_keypair, &prevouts);
+
+    let final_tx = buyer_psbt.extract_tx()?;
+    let attach_commit_tx_hex = hex::encode(serialize_tx(&attach_commit_tx));
+    let raw_attach_reveal_tx_hex = hex::encode(serialize_tx(&attach_reveal_tx));
+    let raw_psbt_hex = hex::encode(serialize_tx(&final_tx));
+
+    let result = client
+        .test_mempool_accept(&[attach_commit_tx_hex, raw_attach_reveal_tx_hex, raw_psbt_hex])
+        .await?;
+
+    assert_eq!(
+        result.len(),
+        3,
+        "Expected exactly three transaction results"
+    );
+    assert!(result[0].reject_reason.is_none());
+    assert!(result[1].reject_reason.is_none());
+    assert!(result[2].reject_reason.is_none());
+    assert!(result[0].allowed);
+    assert!(result[1].allowed);
+    assert!(result[2].allowed);
+
+    Ok(())
+}
