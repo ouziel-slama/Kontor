cd51f4ce4798ff7c5a5f2757a8a2e1c24b6efeb3
wilfreddenton <wilfred@hey.com>
2025-09-04 15:00:50 -0400

    remove commented code

--- core/indexer/src/database/sql/path_prefix_filter_query.sql
+++ core/indexer/src/database/sql/path_prefix_filter_query.sql
@@ -1,19 +1,3 @@
--- SELECT path
--- FROM (
---   SELECT
---     path,
---     height,
---     tx_id,
---     ROW_NUMBER() OVER (PARTITION BY path ORDER BY height DESC, tx_id DESC) AS rank
---   FROM
---     contract_state
---   WHERE
---     contract_id = :contract_id
---     AND path LIKE :path || '%'
---     AND deleted = false
--- ) t
--- WHERE
---   rank = 1;
 SELECT path
 FROM (
   SELECT
