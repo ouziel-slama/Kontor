7966e1c67d7e2fd462c43f1bd0361b41a6f4c31c
wilfreddenton <wilfred@hey.com>
2025-12-15 16:24:00 -0500

    move generation logic out of lib.rs and provide good default contracts_dir for example contracts

--- core/macros/src/lib.rs
+++ core/macros/src/lib.rs
@@ -272,77 +272,7 @@ pub fn test(attr: TokenStream, item: TokenStream) -> TokenStream {
         }
     };
     let func = parse_macro_input!(item as ItemFn);
-
-    let attrs = func.attrs;
-    let fn_name = &func.sig.ident;
-    let fn_generics = &func.sig.generics;
-    let fn_inputs = &func.sig.inputs;
-    let fn_vis = &func.vis;
-    let fn_block = &func.block;
-    let contracts_dir = config.contracts_dir.unwrap_or("./".to_string());
-    let mode = config.mode.unwrap_or("local".to_string());
-
-    let body = if mode == "regtest" {
-        quote! {
-            let (
-                _bitcoin_data_dir,
-                bitcoin_child,
-                bitcoin_client,
-                _kontor_data_dir,
-                kontor_child,
-                kontor_client,
-                identity,
-            ) = RegTester::setup().await?;
-            let result = tokio::spawn({
-                let bitcoin_client = bitcoin_client.clone();
-                let kontor_client = kontor_client.clone();
-                async move {
-                    let mut reg_tester = RegTester::new(identity, bitcoin_client, kontor_client).await?;
-                    let mut runtime = &mut Runtime::new_regtest(RuntimeConfig::builder().contracts_dir(&contracts_dir).build(), reg_tester.clone()).await?;
-                    #fn_block
-                }
-            })
-            .await;
-            RegTester::teardown(bitcoin_client, bitcoin_child, kontor_client, kontor_child).await?;
-            result?
-        }
-    } else {
-        quote! {
-            let mut runtime = &mut Runtime::new_local(RuntimeConfig::builder().contracts_dir(&contracts_dir).build()).await?;
-            #fn_block
-        }
-    };
-
-    let serial = if mode == "regtest" {
-        quote! {
-            #[serial_test::serial]
-        }
-    } else {
-        quote! {}
-    };
-
-    let logging = if config.logging.unwrap_or(false) {
-        quote! {
-            logging();
-        }
-    } else {
-        quote! {}
-    };
-
-    let output = quote! {
-        #[tokio::test]
-        #serial
-        #(#attrs)*
-        #fn_vis async fn #fn_name #fn_generics(#fn_inputs) -> Result<()> {
-            let file_path = absolute_file!();
-            let abs_path = file_path.parent().unwrap();
-            let contracts_dir = abs_path.join(#contracts_dir).to_string_lossy().to_string();
-            #logging
-            #body
-        }
-    };
-
-    output.into()
+    test::generate(config, func).into()
 }
 
 #[proc_macro]
